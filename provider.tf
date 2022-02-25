2fa9075132ae759326c06c4e989a6037b9e076fb
Ullas K <ullaspes93@gmail.com>
2022-02-25 15:59:50 +0530

    added s3 backend for remote state storage

--- provider.tf
+++ provider.tf
@@ -5,6 +5,13 @@ terraform {
       version = "~> 3.0"
     }
   }
+
+  backend "s3" {
+    bucket = "tf-state-assign"
+    key = "dev/terraform.tfstate"
+    region = "us-east-1"
+    dynamodb_table = "tf-state-table"
+  }
 }
 
 provider "aws" {
